4d07a18f6d50fb9fe26ad399ff6a91760ffa313a
admin <zacharyjsanders@gmail.com>
2019-09-02 14:19:34 -0600

    adding testing style

--- wp-flag-staging.php
+++ wp-flag-staging.php
@@ -1,4 +1,15 @@
 <?php
 /**
- * Plugin Name: WP Flag Staging
- */
+* Plugin Name:    WP Flag Staging
+* Plugin URI:     https://zacsanders.com
+* Description:    A WordPress plugin that alerts via color changes to admin dashboard that a staging site is active
+* Version:        1.0
+* Author:         Endurain
+* Author URI:     https://github.com/endurain
+*/
+?>
+
+
+<style>
+#wpadminbar { background: #ff4400!important;}
+</style>
