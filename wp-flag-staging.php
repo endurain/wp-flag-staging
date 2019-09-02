21ecd579009559697f42618b08f8310ae56acbf8
admin <zacharyjsanders@gmail.com>
2019-09-02 16:39:06 -0600

    finalizing plugin

--- wp-flag-staging.php
+++ wp-flag-staging.php
@@ -1,15 +1,24 @@
 <?php
 /**
 * Plugin Name:    WP Flag Staging
-* Plugin URI:     https://zacsanders.com
-* Description:    A WordPress plugin that alerts via color changes to admin dashboard that a staging site is active
+* Description:    A WordPress plugin that alerts via color changes to wp admin bar that a staging site is active
 * Version:        1.0
 * Author:         Endurain
 * Author URI:     https://github.com/endurain
 */
+
+add_action( 'admin_bar_menu', 'toolbar_staging_alert', 999 );
+
+function toolbar_staging_alert( $wp_admin_bar ) {
+	$args = array(
+		'id'    => 'staging_alert',
+		'title' => 'WARNING: STAGING VERSION ACTIVE',
+		'href'  => '',
+		'meta'  => array( 'class' => 'toolbar_staging_alert' )
+	);
+	$wp_admin_bar->add_node( $args );
+}
 ?>
-
-
 <style>
 #wpadminbar { background: #ff4400!important;}
 </style>
