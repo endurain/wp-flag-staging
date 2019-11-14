ed6631dc486455caa0d0f286d5babd87067ee39b
admin <zacharyjsanders@gmail.com>
2019-11-14 15:45:23 -0700

    fixing issue with sitemap for v 1.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wp-flag-staging
+
+Fixed in 1.1: Conflict with Yoast in generating sitemap
 A WordPress plugin that alerts when a staging site is active
--- wp-flag-staging.php
+++ wp-flag-staging.php
@@ -2,8 +2,8 @@
 /**
 * Plugin Name:    WP Flag Staging
 * Description:    A WordPress plugin that alerts via color changes to wp admin bar that a staging site is active
-* Version:        1.0
-* Author:         Endurain
+* Version:        1.1
+* Author:         Zac Sanders
 * Author URI:     https://github.com/endurain
 */
 
@@ -12,16 +12,19 @@ add_action( 'admin_bar_menu', 'toolbar_staging_alert', 999 );
 function toolbar_staging_alert( $wp_admin_bar ) {
 	$args = array(
 		'id'    => 'staging_alert',
-		'title' => 'WARNING: STAGING VERSION ACTIVE',
+		'title' => 'WARNING: STAGING VERSION ACTIVE. DO NOT MAKE CHANGES.',
 		'href'  => '',
 		'meta'  => array( 'class' => 'toolbar_staging_alert' )
 	);
 	$wp_admin_bar->add_node( $args );
 }
-?>
-<style>
-#wpadminbar { background: #ff4400!important;}
-</style>
 
-<!-- todo -->
-<!-- write custom fields without using ACF -->
+// enque stylesheet
+function enqueue_staging_style() {
+            $plugin_url = plugin_dir_url( __FILE__ );
+
+        wp_enqueue_style( 'style',  $plugin_url . "/style.css");
+    }
+
+    add_action( 'admin_print_styles', 'enqueue_staging_style' );
+?>
